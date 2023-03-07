56004e98f1eb8d88ff8ddb6f0a59a94f70252a7c
John M. Wargo <johnwargo@users.noreply.github.com>
2023-03-06 20:02:38 -0500

    all done
    Works

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = eleventyConfig => {
 
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPlugin(pluginRss);
 
 	// From ray camden's blog, first paragraph as excerpt
